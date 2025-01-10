ab51b936641482a392f62f716f8ec667bd6faadb
deltag0 <110630642+deltag0@users.noreply.github.com>
2025-01-10 08:30:00 -0500

    First commit

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,22 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    event = 'VeryLazy',
+    config = function()
+      require 'null-ls'
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'clangd',
+        'clang-format',
+      },
+    },
+  },
+}
