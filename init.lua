b6741dd089faa84a2e2410f83a8580820f51ee98
deltag0 <110630642+deltag0@users.noreply.github.com>
2025-03-14 14:01:31 -0400

    update

--- init.lua
+++ init.lua
@@ -538,7 +538,24 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        -- jedi_language_server = {
+        --   capabilities = capabilities,
+        --   cmd = { 'jedi-language-server' }, -- Ensure this binary is installed
+        --   settings = {
+        --     jedi = {
+        --       completion = {
+        --         disableSnippets = true, -- Prevents snippet-like completions
+        --       },
+        --       diagnostics = {
+        --         enable = true,
+        --         didOpen = true,
+        --         didChange = true,
+        --       },
+        --     },
+        --   },
+        -- },
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -585,12 +602,23 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            if server_name == 'clangd' then
+              server.cmd = {
+                'clangd',
+              }
+            end
+
             require('lspconfig')[server_name].setup(server)
           end,
+          -- ['clangd'] = function()
+          --   require('lspconfig').clangd.setup {
+          --     on_attach = function(client, bufnr)
+          --       vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, silent = true })
+          --       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, silent = true })
+          --     end,
+          --   }
+          -- end,
         },
       }
     end,
@@ -639,122 +667,7 @@ require('lazy').setup({
     },
   },
 
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<Tab>'] = cmp.mapping.confirm { select = true },
-          ['<C-e'] = cmp.mapping.close(),
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
-
-        sources = {
-          {
-            name = 'lazydev',
-            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-            group_index = 0,
-          },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
+  -- Autocompletion
 
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
@@ -812,14 +725,60 @@ require('lazy').setup({
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
+  }, -- Autocomplete setup
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- 'rafamadriz/friendly-snippets' if you need extra snippets
+        },
+      },
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+    },
+    config = function()
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
+      luasnip.config.setup {}
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = 'menu,menuone,noinsert' },
+        mapping = cmp.mapping.preset.insert {
+          -- Define your key mappings here (e.g., <Tab>, <C-n>, <C-p>, etc.)
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
+        },
+        sources = {
+          { name = 'lazydev' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'path' },
+        },
+      }
+    end,
   },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -969,8 +928,6 @@ require('lazy').setup({
 -- vim: ts=2 sts=2 sw=2 et
 vim.api.nvim_set_keymap('i', '<M-Space>', '<Esc>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'ds', ':lua delete_and_change()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'i', ':lua insert_move()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'df', ':lua delete_and_change_front()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'da', ':lua delete_and_change_no_insert()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'dc', ':lua delete_and_change_front_no_insert()<CR>', { noremap = true, silent = true })
 function delete_and_change()
@@ -978,15 +935,18 @@ function delete_and_change()
   vim.cmd 'normal! cw'
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i<Right>', true, false, true), 'n', true)
 end
+
 function delete_and_change_front()
   vim.cmd 'normal! dw'
   vim.cmd 'normal! cw'
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i<Right>', true, false, true), 'n', true)
 end
+
 function delete_and_change_front_no_insert()
   vim.cmd 'normal! dw'
   vim.cmd 'normal! cw'
 end
+
 function delete_and_change_no_insert()
   vim.cmd 'normal! db'
   vim.cmd 'normal! cw'
@@ -995,3 +955,6 @@ end
 function insert_move()
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i<Right>', true, false, true), 'n', true)
 end
+
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.number = true -- Enable absolute line numbers for the current line
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,6 +16,7 @@ return {
       ensure_installed = {
         'clangd',
         'clang-format',
+        'pyright',
       },
     },
   },
